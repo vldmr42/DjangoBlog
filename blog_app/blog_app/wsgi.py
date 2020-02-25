1118871c7f031fc6d2a2f20448727cce72ef9799
manemejeff <kmb14056@yandex.ru>
2020-02-25 20:10:07 +0400

    renamed app for greater good

--- blog_app/blog_app/wsgi.py
+++ blog_app/blog_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WishListApp project.
+WSGI config for blog_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WishListApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_app.settings')
 
 application = get_wsgi_application()
