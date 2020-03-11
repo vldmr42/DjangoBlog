a18f0abe8b80a2fa99316641d3cf0144b87273f6
manemejeff <kmb14056@yandex.ru>
2020-03-11 18:05:51 +0400

    list of items on a page by cards

--- blog_app/wishlist/views.py
+++ blog_app/wishlist/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import WishListItem
 
 
 # Create your views here.
 
 class WishListView(ListView):
-    # model = Post
+    model = WishListItem
     template_name = 'wishlist/index.html'
-    context_object_name = 'posts'
+    context_object_name = 'items'
     ordering = ['-date_posted']
     paginate_by = 5
